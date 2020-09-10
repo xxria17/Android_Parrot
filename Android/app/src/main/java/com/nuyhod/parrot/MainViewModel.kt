5099cc6d136d6e257adfdf3ffc9633abc6ed3bee
Dohyun Lee <xxrcy32@gmail.com>
2020-09-10 15:17:09 +0900

    데이터 받아와서 패캐지명 구분

---
+++ Android/app/src/main/java/com/nuyhod/parrot/MainViewModel.kt
@@ -0,0 +1,23 @@
+package com.nuyhod.parrot
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+
+class MainViewModel : ViewModel() {
+    private lateinit var tts: BaseTTS
+
+    private fun filterPck(msgModel: MsgModel): String {
+        if (msgModel.pckName?.equals(R.string.kakao_talk)!! || msgModel.pckName?.equals(R.string.instagram)!! || msgModel.pckName?.equals(
+                R.string.message
+            )!! ||
+            msgModel.pckName?.equals(R.string.face_book)!!
+        ) {
+            return "${msgModel.sender} 님이 메세지를 보냈습니다. \n ${msgModel.text}"
+        } else {
+            return "${msgModel.text}"
+        }
+    }
+
+}
